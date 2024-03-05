1945dfdc718f60917d0ba07582ce853151b1abc2
Szymon Andrzejewski <monandszy@gmail.com>
2024-03-05 22:58:07 +0100

    feat: README update

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # FoodDelivery
+
 A simple practice project
+
+### Features as Seller:
+
+- CRUD Restaurants - Address is assigned based on your ip
+- CRUD Menus
+- CRUD MenuPositions - Add a picture to each one
+- See Complete and Incomplete Orders - Complete Orders
+
+### Features as Client:
+
+- Find restaurants nearest to you based on ip and restaurant's available delivery range
+- Read Menus
+- Read MenuPositions - Create Order
+- Read Your Orders and Their Status - Cancel Order within 20 minutes
+
+### Features as Admin:
+
+- CRUD Accounts - assign roles
+
+### Rest API Features:
+
+- Create a full restaurant in one command
+- CRUD Orders (as a Client)
+
+#### Component Diagram
+
+<img src="/diagrams/component.svg" alt="image" height="500">
+
+#### ERD Diagram
+
+<img src="/diagrams/erd.png" alt="image" height="500">
+
+#### Jacoco Test Coverage
+
+<img src="/diagrams/jacoco.png" alt="image">
--- src/main/java/code/PlantUmlCreator.java
+++ src/main/java/code/PlantUmlCreator.java
@@ -16,70 +16,75 @@ import java.util.Optional;
 
 public class PlantUmlCreator {
 
-    // every ..|> to ..u|>
-    private static final String STARTUML_TO_REPLACE = "@startuml";
-    private static final String STARTUML_REPLACEMENT = """
-            @startuml
-            !theme vibrant
-            skinparam classAttributeIconSize 0
-            left to right direction
-            """;
+   // every ..|> to ..u|>
+   private static final String STARTUML_TO_REPLACE = "@startuml";
+   private static final String STARTUML_REPLACEMENT = """
+       @startuml
+       !theme vibrant
+       skinparam classAttributeIconSize 0
+       left to right direction
+       """;
 
-    private static final String SAVE_DIRECTORY = "diagrams";
+   private static final String SAVE_DIRECTORY = "diagrams";
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 //        Optional<String> s = createSequenceDiagram(MortgageCalculationService.class, "calculate");
-        Optional<String> diagramText = createClassDiagram("code");
-        saveToFile(diagramText, "ClassDiagram.puml");
+      List<String> include = List.of(
+          "code/component/manageAccount",
+          "code/component/manageRestaurant",
+          "code/component/manageOrder"
+      );
+      String hideRegex = "(code.component.manageAccount)|(manageOrder)|(manageRestaurant)"; // Doesnt work...
+      Optional<String> diagramText = createClassDiagram(include, hideRegex);
+      saveToFile(diagramText, "ClassDiagram.puml");
 
-    }
+   }
 
-    private static Optional<String> createSequenceDiagram(Class inputClass, String method) {
-        var builder = new PlantUMLSequenceDiagramConfigBuilder(
-                inputClass.getName(),
-                method);
-        var generator = new PlantUMLSequenceDiagramGenerator(builder.build());
-        try {
-            return Optional.of(generator.generateDiagramText());
-        } catch (NotFoundException e) {
-            System.err.printf("Exception while generating SequenceDiagram: %s", e.getMessage());
-        }
-        return Optional.empty();
-    }
+   private static Optional<String> createSequenceDiagram(Class inputClass, String method) {
+      var builder = new PlantUMLSequenceDiagramConfigBuilder(
+          inputClass.getName(),
+          method);
+      var generator = new PlantUMLSequenceDiagramGenerator(builder.build());
+      try {
+         return Optional.of(generator.generateDiagramText());
+      } catch (NotFoundException e) {
+         System.err.printf("Exception while generating SequenceDiagram: %s", e.getMessage());
+      }
+      return Optional.empty();
+   }
 
-    private static Optional<String> createClassDiagram(String packageName) {
-        var config = new PlantUMLClassDiagramConfigBuilder(List.of(packageName))
-                .withRemoveFields(true)
-                .withHideMethods(true)
-                .build();
-        var generator = new PlantUMLClassDiagramGenerator(config);
-        return Optional.of(generator.generateDiagramText()
-                .replace(STARTUML_TO_REPLACE, STARTUML_REPLACEMENT)
-                .replace(packageName + ".", "")
-        );
-    }
+   private static Optional<String> createClassDiagram(List<String> include, String blacklistRegex) {
+      var config = new PlantUMLClassDiagramConfigBuilder(blacklistRegex, include)
+          .withRemoveFields(true)
+          .withHideMethods(true)
+          .build();
+      var generator = new PlantUMLClassDiagramGenerator(config);
+      return Optional.of(generator.generateDiagramText()
+          .replace(STARTUML_TO_REPLACE, STARTUML_REPLACEMENT)
+      );
+   }
 
-    private static void saveToFile(Optional<String> diagramText, String fileName) {
-        if (diagramText.isPresent()) {
-            try {
-                writeToFile(diagramText.get(), fileName);
-            } catch (IOException e) {
-                System.err.printf("Exception while saving to file: %s", e.getMessage());
-            }
-        }
-    }
+   private static void saveToFile(Optional<String> diagramText, String fileName) {
+      if (diagramText.isPresent()) {
+         try {
+            writeToFile(diagramText.get(), fileName);
+         } catch (IOException e) {
+            System.err.printf("Exception while saving to file: %s", e.getMessage());
+         }
+      }
+   }
 
-    private static void writeToFile(String generatedDiagram, String fileName) throws IOException {
-        Path directory = Paths.get(".")
-                .toRealPath()
-                .resolve(Paths.get(SAVE_DIRECTORY));
-        if (!Files.exists(directory)) {
-            Files.createDirectories(directory);
-        }
-        try (
-                BufferedWriter writer = Files.newBufferedWriter(directory.resolve(fileName))
-        ) {
-            writer.write(generatedDiagram);
-        }
-    }
+   private static void writeToFile(String generatedDiagram, String fileName) throws IOException {
+      Path directory = Paths.get(".")
+          .toRealPath()
+          .resolve(Paths.get(SAVE_DIRECTORY));
+      if (!Files.exists(directory)) {
+         Files.createDirectories(directory);
+      }
+      try (
+          BufferedWriter writer = Files.newBufferedWriter(directory.resolve(fileName))
+      ) {
+         writer.write(generatedDiagram);
+      }
+   }
 }
